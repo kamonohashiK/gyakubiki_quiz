e51da48de8888e2e16055f9cee520c70c163fc26
門屋弘毅 <>
2021-06-30 00:06:36 +0900

    例題のリンクを名前付きルートに変更

--- backend/resources/views/top.blade.php
+++ backend/resources/views/top.blade.php
@@ -1,9 +1,10 @@
 <h1>top</h1>
+
 <h3>最近追加された問題の答え</h3>
 <ul>
-    <li><a href="/questions?answer=hoge">hoge</a></li>
-    <li><a href="/questions?answer=愛媛県">愛媛県</a></li>
-    <li><a href="/questions?answer=ランドルト環">ランドルト環</a></li>
-    <li><a href="/questions?answer=新垣結衣">新垣結衣</a></li>
-    <li><a href="/questions?answer=15">15</a></li>
+    <li><a href="{{route('questions.index', ['answer' => 'hoge'])}}">hoge</a></li>
+    <li><a href="{{route('questions.index', ['answer' => '愛媛県'])}}">愛媛県</a></li>
+    <li><a href="{{route('questions.index', ['answer' => 'ランドルト環'])}}">ランドルト環</a></li>
+    <li><a href="{{route('questions.index', ['answer' => '新垣結衣'])}}">新垣結衣</a></li>
+    <li><a href="{{route('questions.index', ['answer' => '15'])}}">15</a></li>
 </ul>
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TopController;
 use App\Http\Controllers\QuestionController;
 
-Route::get('/', [TopController::class, 'top']);
+Route::get('/', [TopController::class, 'top'])->name('top');
 
-Route::get('/questions', [QuestionController::class, 'index']);
+Route::get('/questions', [QuestionController::class, 'index'])->name('questions.index');
