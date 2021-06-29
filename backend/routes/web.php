f0aa2c08628b1a470c2f90ccb1e5f56c2510d1c9
門屋弘毅 <>
2021-06-29 23:33:46 +0900

    コントローラに処理を分離

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,22 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TopController;
+use App\Http\Controllers\QuestionController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [TopController::class, 'top']);
 
-Route::get('/', function () {
-    return view('top');
-});
-
-Route::get('/questions', function () {
-    return view('questions');
-});
+Route::get('/questions', [QuestionController::class, 'index']);
