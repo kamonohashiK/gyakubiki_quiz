0067d25bcceac9561fee2abbafcf72d003a4bbff
門屋弘毅 <>
2021-07-03 09:43:47 +0900

    答えの一覧をバッジに置き換え、コンポーネントに切り出し

--- backend/resources/views/top.blade.php
+++ backend/resources/views/top.blade.php
@@ -5,9 +5,5 @@
 
 <h3>最近追加された問題の答え</h3>
 
-<ul>
-@foreach($answers as $a)
-    <li><a href="{{route('questions.index', ['answer' => $a->name])}}">{{$a->name}}</a></li>
-@endforeach
-</ul>
+<x-badges :answers="$answers" />
 @endsection
