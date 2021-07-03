ed4a43c0019200c665944b5a36bfb662a3afe51e
門屋弘毅 <>
2021-07-03 09:33:21 +0900

    アラートをコンポーネントに切り出し

--- backend/resources/views/layouts/layout.blade.php
+++ backend/resources/views/layouts/layout.blade.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>タイトル</title>
+        <title>タイトル</title>{{--TODO:ページによって入れ替えたい--}}
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
@@ -13,9 +13,7 @@
         <x-header />
 
         <div class="main">
-            @if(session('success'))
-            <p> {{ session('success') }}</p>
-            @endif
+            <x-alert />
             @yield('content')
         </div>
     </body>
